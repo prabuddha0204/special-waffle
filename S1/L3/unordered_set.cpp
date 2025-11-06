54baff5b10949766287efab53590971703493244
Prabuddha Saxena <prabuddha02.04@gmail.com>
2025-11-06 16:25:25 +0530

    UO SET

--- S1/L3/unordered_set.cpp
+++ S1/L3/unordered_set.cpp
@@ -1,1 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    unordered_set<int> s;
+    for (int i = 1; i <= 10; i++)
+    {
+        s.insert(i);
+    }
+    cout << "ELEMENTS IN SET ARE: ";
+    for (int x : s)
+    {
+        cout << x << " ";
+    }
+    int p = 2;
+
+    if (s.find(p) != s.end())
+    {
+        cout << "ELEMENT EXISTS" << endl;
+    }
+    else
+    {
+        cout << "ELEMENT DOESNT EXIST";
+    }
+    s.erase(s.begin());
+    for (int x : s)
+    {
+        cout << x << " ";
+    }
+    s.clear();
+    cout << endl;
+    cout << s.size();
+}
